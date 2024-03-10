70d7d0e9f5b7c36049383ded54dec6762c8a2a17
Tan Chee Boon <tan.cb@aem.com.sg>
2024-03-10 17:01:07 +0800

    initial database exploration

---
+++ ChequeWriter.DatabaseInitializer/ChequeWriterDbContext.cs
@@ -0,0 +1,22 @@
+﻿using ChequeWriter.DatabaseInitializer.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ChequeWriter.DatabaseInitializer;
+
+public class ChequeWriterDbContext : DbContext
+{
+    public ChequeWriterDbContext(DbContextOptions<ChequeWriterDbContext> options) :
+        base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Cheque>()
+            .Property(d => d.DateCreated)
+            .HasDefaultValue("CURRENT_TIMESTAMP")
+            .HasColumnType("timestamp without time zone");
+    }
+
+    public DbSet<Cheque> Cheques { get; set; }
+}
---
+++ ChequeWriter.DatabaseInitializer/Models/Cheque.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ChequeWriter.DatabaseInitializer.Models
+{
+    public class Cheque
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Amount { get; set; }
+        public DateTime DateCreated { get; set; }
+    }
+}
