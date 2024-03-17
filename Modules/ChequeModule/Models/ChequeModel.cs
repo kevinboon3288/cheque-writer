646ec8964c43109bc8267ccb70ccd8f2d4945617
Yawhou <54216858+Yawhou@users.noreply.github.com>
2024-03-17 17:22:46 +0800

    Created ChequeModel class with properties defined

---
+++ Modules/ChequeModule/Models/ChequeModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChequeWriter.Modules.ChequeModule.Models
+{
+    public class ChequeModel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public double Amount { get; set; }
+
+        public DateTime DateCreated { get; set; }
+
+        public string AmountInWord { get; set; }
+
+        public ChequeModel(string name, string amtInWord)
+        {
+            Name = name;
+            AmountInWord = amtInWord;
+        }
+    }
+}
