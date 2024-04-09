68418c4e1eb5ab1a3b087d24728e1d78fce50499
Tan Chee Boon <tan.cb@aem.com.sg>
2024-04-09 19:11:43 +0800

    implement dataContextFactory to dataService

---
+++ Services/DataServices/ChequeWriterDbContext.cs
@@ -0,0 +1,15 @@
+﻿namespace DataServices;
+
+public class ChequeWriterDbContext: DbContext
+{
+    public DbSet<Cheque> Cheque => Set<Cheque>();
+
+    public ChequeWriterDbContext() : base() { }
+
+    public ChequeWriterDbContext(DbContextOptions<ChequeWriterDbContext> options) : base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+    }
+}
--- Services/DataServices/Using.cs
+++ Services/DataServices/Using.cs
@@ -3,3 +3,7 @@ global using System.Collections.Generic;
 global using System.Linq;
 global using System.Text;
 global using System.Threading.Tasks;
+global using System.ComponentModel.DataAnnotations;
+global using Microsoft.EntityFrameworkCore;
+global using Microsoft.EntityFrameworkCore.Design;
+global using DataServices.Models;
