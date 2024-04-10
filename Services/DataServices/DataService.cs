62690c0b83410f3659edb462821e4dd3f6334cf9
Tan Chee Boon <tan.cb@aem.com.sg>
2024-04-10 22:48:09 +0800

    code clean up

--- Services/DataServices/DataService.cs
+++ Services/DataServices/DataService.cs
@@ -2,23 +2,23 @@
 
 public class DataService
 {
-    //private const string _connectionString = "Host=localhost;Database=cheque-writer-ui;Username=postgres;Password=postgres";
-    //private readonly IDesignTimeDbContextFactory<ChequeWriterDbContext> _dbContextFactory;
+    private const string _connectionString = "Host=localhost;Database=cheque-writer-ui;Username=postgres;Password=postgres";
+    private readonly IDesignTimeDbContextFactory<ChequeWriterDbContext> _dbContextFactory;
 
-    //public DataService(IDesignTimeDbContextFactory<ChequeWriterDbContext> dbContextFactory)
-    //{
-    //    _dbContextFactory = dbContextFactory;
-    //}
+    public DataService(IDesignTimeDbContextFactory<ChequeWriterDbContext> dbContextFactory)
+    {
+        _dbContextFactory = dbContextFactory;
+    }
 
-    //public Cheque? GetCheque(int id) 
-    //{
-    //    using ChequeWriterDbContext db = _dbContextFactory.CreateDbContext([_connectionString]);
+    public Cheque? GetCheque(int id)
+    {
+        using ChequeWriterDbContext db = _dbContextFactory.CreateDbContext([_connectionString]);
 
-    //    var queryResult =
-    //        from c in db.Cheque.ToList()
-    //        where c.Id == id
-    //        select c;
+        var queryResult =
+            from c in db.Cheque.ToList()
+            where c.Id == id
+            select c;
 
-    //    return queryResult.FirstOrDefault();
-    //}
+        return queryResult.FirstOrDefault();
+    }
 }
--- Services/DataServices/Using.cs
+++ Services/DataServices/Using.cs
@@ -2,4 +2,5 @@
 global using System.ComponentModel.DataAnnotations.Schema;
 global using System.ComponentModel.DataAnnotations;
 global using Microsoft.EntityFrameworkCore;
+global using Microsoft.EntityFrameworkCore.Design;
 global using DataServices.Models;
