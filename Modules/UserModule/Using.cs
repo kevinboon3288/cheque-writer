9fa1101837e662b56b1d37709fca6ff38dad6ee3
Tan Chee Boon <tan.cb@aem.com.sg>
2024-04-13 17:00:06 +0800

    add user module project

---
+++ Modules/UserModule/Using.cs
@@ -0,0 +1,12 @@
+﻿global using System;
+global using System.Collections.Generic;
+global using System.Linq;
+global using System.Text;
+global using System.Threading.Tasks;
+global using Prism.Ioc;
+global using Prism.Modularity;
+global using Prism.Mvvm;
+global using Prism.Regions;
+global using Prism.Commands;
+global using ChequeWriter.Modules.UserModule.Views;
+global using ChequeWriter.Modules.UserModule.ViewModels;
---
+++ Modules/UserModule/ViewModels/UserViewModel.cs
@@ -0,0 +1,36 @@
+﻿namespace ChequeWriter.Modules.UserModule.ViewModels
+{
+    public class UserViewModel: BindableBase, INavigationAware
+    {
+        private readonly IRegionManager _regionManager;
+
+        public DelegateCommand NavigateBack { get; set; }
+
+        public UserViewModel(IRegionManager regionManager)
+        {
+            _regionManager = regionManager;
+
+            NavigateBack = new DelegateCommand(OnReturn);
+        }
+
+        private void OnReturn()
+        {
+            IRegion region = _regionManager.Regions["MainContentRegion"];
+            region.RequestNavigate("MainView");
+        }
+
+        public void OnNavigatedTo(NavigationContext navigationContext)
+        {
+            // Entry Point when navigate to ReportView
+        }
+
+        public bool IsNavigationTarget(NavigationContext navigationContext)
+        {
+            return true;
+        }
+
+        public void OnNavigatedFrom(NavigationContext navigationContext)
+        {
+        }
+    }
+}
