d24f54b9a70e76f70084e67fa87f22ecaa6a662c
Tan Chee Boon <tan.cb@aem.com.sg>
2024-05-01 10:06:20 +0800

    Add DataServiceUnitTests

---
+++ UnitTest/DataServiceUnitTests/UserDataServiceUnitTest.cs
@@ -0,0 +1,143 @@
+namespace DataServiceUnitTests;
+
+[TestFixture]
+public class UserDataServiceUnitTest
+{
+    [SetUp]
+    public void SetUp()
+    {
+
+    }
+
+    [Test]
+    public void GetUserLevelNameById_ReturnString_Success()
+    {
+        // Arrange
+
+        // Act
+
+        // Assert
+        Assert.Pass();
+    }
+
+    [Test]
+    public void GetUserLevelNameById_ReturnString_Fail()
+    {
+        // Arrange
+
+        // Act
+
+        // Assert
+        Assert.Pass();
+    }
+
+    [Test]
+    public void GetUserLevels_ReturnAllUserLevel_Success()
+    {
+        // Arrange
+
+        // Act
+
+        // Assert
+        Assert.Pass();
+    }
+
+    [Test]
+    public void GetUserLevels_ReturnAllUserLevel_Null()
+    {
+        // Arrange
+
+        // Act
+
+        // Assert
+        Assert.Pass();
+    }
+
+    [Test]
+    public void IsValidUser_ReturnTrue_Success()
+    {
+        // Arrange
+
+        // Act
+
+        // Assert
+        Assert.Pass();
+    }
+
+    [Test]
+    public void IsValidUser_ReturnFalse_Success()
+    {
+        // Arrange
+
+        // Act
+
+        // Assert
+        Assert.Pass();
+    }
+
+    [Test]
+    public void GetAllUsers_ReturnAllUsers_Success()
+    {
+        // Arrange
+
+        // Act
+
+        // Assert
+        Assert.Pass();
+    }
+
+    [Test]
+    public void GetAllUsers_ReturnAllUsers_Null()
+    {
+        // Arrange
+
+        // Act
+
+        // Assert
+        Assert.Pass();
+    }
+
+    [Test]
+    public void AddUser_ReturnUserId_Success()
+    {
+        // Arrange
+
+        // Act
+
+        // Assert
+        Assert.Pass();
+    }
+
+    [Test]
+    public void AddUser_ThrowDataServiceException_Success()
+    {
+        // Arrange
+
+        // Act
+
+        // Assert
+        Assert.Pass();
+    }
+
+    [Test]
+    public void DeleteUser_ReturnResultValue_Success()
+    {
+        // Arrange
+
+        // Act
+
+        // Assert
+        Assert.Pass();
+    }
+
+    [Test]
+    public void DeleteUser_ThrowDataServiceException_Success()
+    {
+        // Arrange
+
+        // Act
+
+        // Assert
+        Assert.Pass();
+    }
+}
---
+++ UnitTest/DataServiceUnitTests/Using.cs
@@ -0,0 +1,4 @@
+﻿global using DataServices;
+global using NSubstitute;
+global using NUnit.Framework;
+global using System;
