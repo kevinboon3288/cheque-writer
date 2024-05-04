c13c3618e2a70f0982592c4acfc76a31ee803c04
Yawhou <54216858+Yawhou@users.noreply.github.com>
2024-05-05 01:22:36 +0800

    Remove code that was added by accident

--- Modules/ChequeModule/Views/ChequeView.xaml.cs
+++ Modules/ChequeModule/Views/ChequeView.xaml.cs
@@ -24,10 +24,5 @@ namespace ChequeWriter.Modules.ChequeModule.Views
         {
             InitializeComponent();
         }
-
-        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
-        {
-
-        }
     }
 }
