fc14ecf063d6fc65657fa144c5d0af60bdae3d4e
Yawhou <54216858+Yawhou@users.noreply.github.com>
2024-06-15 03:59:11 +0800

    Update ChequeViewModel.cs

--- Modules/ChequeModule/ViewModels/ChequeViewModel.cs
+++ Modules/ChequeModule/ViewModels/ChequeViewModel.cs
@@ -7,7 +7,38 @@ public class ChequeViewModel : BindableBase, INavigationAware
     private readonly IChequeManager _chequeManager;
     private readonly IEventAggregator _eventAggregator;
 
+    private string? _payeeName;
+    private string? _amount;
+    private DateOnly? _date;
+    private string? _drawer;
+
+    public string? PayeeName
+    {
+        get { return _payeeName; }
+        set { SetProperty(ref _payeeName, value); }
+    }
+
+    public string? Amount
+    {
+        get { return _amount; }
+        set { SetProperty(ref _amount, value); }
+    }
+
+    public DateOnly? Date
+    {
+        get { return _date; }
+        set { SetProperty(ref _date, value); }
+    }
+
+    public string? Drawer
+    {
+        get { return _drawer; }
+        set { SetProperty(ref _drawer, value); }
+    }
+
     public DelegateCommand NavigateBack { get; set; }
+    public DelegateCommand CancelCommand { get; private set; }
+    public DelegateCommand SaveFormCommand { get; private set; }
 
     public ChequeViewModel(IRegionManager regionManager, IChequeManager chequeManager, IEventAggregator eventAggregator)
     {
@@ -16,6 +47,9 @@ public class ChequeViewModel : BindableBase, INavigationAware
         _eventAggregator = eventAggregator;
 
         NavigateBack = new DelegateCommand(OnReturn);
+        CancelCommand = new DelegateCommand(OnReturn);
+        SaveFormCommand = new DelegateCommand(OnSaveForm);
+
     }
 
     private void OnReturn()
@@ -36,5 +70,10 @@ public class ChequeViewModel : BindableBase, INavigationAware
 
     public void OnNavigatedFrom(NavigationContext navigationContext)
     {
+    }
+
+    private void OnSaveForm()
+    {
+
     }
 }
