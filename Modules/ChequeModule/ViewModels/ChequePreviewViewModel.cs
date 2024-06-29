eb12c5342e2cace53e4460af3c57fe92f8f7276a
Tan Chee Boon <kevintan7227@gmail.com>
2024-06-29 13:48:57 +0800

    create events for trasmitting the preview data and create its view model for binding purpose

--- Modules/ChequeModule/ViewModels/ChequePreviewViewModel.cs
+++ Modules/ChequeModule/ViewModels/ChequePreviewViewModel.cs
@@ -1,16 +1,79 @@
-﻿namespace ChequeWriter.Modules.ChequeModule.ViewModels;
+﻿using System.Globalization;
+
+namespace ChequeWriter.Modules.ChequeModule.ViewModels;
 
 public class ChequePreviewViewModel: BindableBase, INavigationAware
 {
     private readonly IRegionManager _regionManager;
+    private readonly IEventAggregator _eventAggregator;
 
-    public ChequePreviewViewModel(IRegionManager regionManager)
+    private string? _dateCreated;
+    private string? _payee;
+    private double? _amount;
+    private string? _amountInWords;
+
+    public string? DateCreated
+    {
+        get { return _dateCreated; }
+        set { SetProperty(ref _dateCreated, value); }
+    }
+
+    public string? Payee
+    {
+        get { return _payee; }
+        set { SetProperty(ref _payee, value); }
+    }
+
+    public double? Amount
+    {
+        get { return _amount; }
+        set { SetProperty(ref _amount, value); }
+    }
+
+    public string? AmountInWords
+    {
+        get { return _amountInWords; }
+        set { SetProperty(ref _amountInWords, value); }
+    }
+
+    public ChequePreviewViewModel(IRegionManager regionManager, IEventAggregator eventAggregator)
     {
         _regionManager = regionManager;
+        _eventAggregator = eventAggregator;
+
+        _eventAggregator.GetEvent<PreviewChequeEvent>().Subscribe(OnChequeInfoReceived);
     }
 
     public void OnNavigatedTo(NavigationContext navigationContext)
     {
+        OnRefresh();
+    }
+
+    private void OnRefresh() 
+    {
+        DateCreated = null;
+        Payee = null;
+        Amount = null;
+        AmountInWords = null;
+    }
+
+    private void OnChequeInfoReceived(Dictionary<string, dynamic> chequeEventArgs) 
+    {
+        if (chequeEventArgs == null) 
+        {
+            return;
+        }
+
+        if (chequeEventArgs.TryGetValue("Payee", out dynamic? payee) &&
+            chequeEventArgs.TryGetValue("Amount", out dynamic? amount) &&
+            chequeEventArgs.TryGetValue("AmountInWords", out dynamic? amountInWords) &&
+            chequeEventArgs.TryGetValue("DateCreated", out dynamic? dateCreated)) 
+        {
+            Payee = payee;
+            Amount = (double)amount;
+            AmountInWords = amountInWords;
+            DateCreated = dateCreated;
+        }
     }
 
     public bool IsNavigationTarget(NavigationContext navigationContext)
