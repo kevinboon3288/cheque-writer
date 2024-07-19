fad4a42a0502715006d78c918f95d58f6b29e2a6
Yawhou <54216858+Yawhou@users.noreply.github.com>
2024-07-19 20:43:57 +0800

    Remove constructor of Cheque.cs

--- Modules/ChequeModule/Models/Cheque.cs
+++ Modules/ChequeModule/Models/Cheque.cs
@@ -5,12 +5,5 @@ public class Cheque
     public int Id { get; set; }
     public required string Name { get; set; }
     public required double Amount { get; set; }
-    public required DateTime DateCreated { get; set; }
-
-    public Cheque(string name, double amount)
-    {
-        Name = name;
-        Amount = amount;
-        DateCreated = DateTime.UtcNow;
-    }
+    public required DateTime? DateCreated { get; set; } 
 }
